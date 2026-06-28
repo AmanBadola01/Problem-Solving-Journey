cb6cb298edf9b686b30d4763ee94e0f25b26edf4
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-06-28 10:06:40 +0530

    ✅ Customer Who Visited but Did Not Make Any Transactions | Easy | SQL | Runtime: 117 ms | Memory: 0B | 2026-06-28

--- leetcode/easy/1581-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ leetcode/easy/1581-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,6 +1,6 @@
-select v.customer_id , count(customer_id) as count_no_trans 
-from Visits as v
-LEFT JOIN Transactions as t
-on v.visit_id = t.visit_id
-where t.transaction_id IS NULL
-GROUP BY t.transaction_id 
+SELECT v.customer_id, COUNT(customer_id) as count_no_trans 
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
