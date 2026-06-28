0b0aa680c07640ebca2e80cb54aaf287c6e74751
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-06-28 10:18:18 +0530

    ✅ Rising Temperature | Easy | SQL | Runtime: 84 ms | Memory: 0B | 2026-06-28

--- leetcode/easy/0197-rising-temperature/solution.sql
+++ leetcode/easy/0197-rising-temperature/solution.sql
@@ -1,5 +1,5 @@
-select b.id
+select a.id
 from Weather as a
 INNER JOIN Weather as b
 WHERE DATEDIFF(a.recordDate, b.recordDate) =1 
-AND b.temperature  > a.temperature 
+AND a.temperature  > b.temperature 
