56e0bea0e487d6f837c5bf3e6b109ab41cbeca71
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-06-29 08:59:32 +0530

    ✅ Average Time of Process per Machine | Easy | SQL | Runtime: 73 ms | Memory: 0B | 2026-06-29

--- leetcode/easy/1661-average-time-of-process-per-machine/solution.sql
+++ leetcode/easy/1661-average-time-of-process-per-machine/solution.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-select a1.machine_id, a1.timestamp, a2.timestamp
+select a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) as processing_time 
 from Activity as a1
 INNER JOIN Activity as a2
 ON a1.machine_id = a2.machine_id 
 AND a1.process_id = a2.process_id 
 AND a1.timestamp < a2.timestamp
+GROUP BY machine_id
