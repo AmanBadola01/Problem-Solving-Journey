6e1e22b4e4b592a0eeae560786c5717958ea3c6f
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-06-29 09:43:19 +0530

    Delete unnecessary files

--- leetcode/easy/0577-employee-bonus/solution.sql
+++
@@ -1,5 +0,0 @@
-Select e.name, b.bonus
-from Employee as e
-LEFT JOIN Bonus as b
-ON e.empID = b.empID
-WHERE bonus IS NULL OR bonus < 1000;
--- leetcode/easy/1068-product-sales-analysis-i/solution.sql
+++
@@ -1,4 +0,0 @@
-select product_name, year, price 
-from Sales as s
-left join Product as p
-on s.product_id  = p.product_id 
