9ec909e33fc59de3309c457d79b1c437765d91c3
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-06-30 16:26:55 +0530

    ✅ Two Sum | Easy | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-06-30

---
+++ leetcode/easy/0001-two-sum/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        start = 1
+        for i in range(len(nums)-1):
+            if nums[start] + nums[i] == target:
+                result.append(start)
+                result.append(i)
+            else:
+                start += 1
+        return result
