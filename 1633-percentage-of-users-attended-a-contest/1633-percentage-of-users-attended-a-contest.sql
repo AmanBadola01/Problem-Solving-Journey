10b450b486e747c5907a51f82882fe52dde5d05b
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-07-07 10:38:12 +0530

    Time: 5660 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+select r.contest_id, 
+ROUND(COUNT(r.user_id) * 100.0 /(SELECT COUNT(*) FROM Users), 2) AS percentage
+from Register r
+group by r.contest_id
+order by percentage desc ,contest_id asc 
