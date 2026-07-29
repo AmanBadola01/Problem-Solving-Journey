e251ee5aace91bd352428a72329277ffc98e2509
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-07-29 09:30:17 +0530

    ✅ GSQ03E | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-29

---
+++ codechef/medium/GSQ03E/solution.cpp
@@ -0,0 +1,5 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+
+Insert into employee (employee_id, employee_name, department) 
+values (4, 'Marcus Garcia', 'Product'),
+       (5, 'Samantha Park', 'Hr');
