d6bca5a2f0418f035ee3753d53d58e3c469a3484
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-03 11:35:08 +0530

    ✅ APYXAM01 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/APYXAM01/solution.sql
+++ codechef/medium/APYXAM01/solution.sql
@@ -1,15 +1,15 @@
 -- write your code here
 
 create table Dogs(
-    dog_id INT primary key, 
-    name varchar(50) unique,
-    breed varchar(50),
-    age int check(age >= 1),
-    weight_kg float,
-    check(breed IN ('Beagle', 'Labrador', 'Poodle', 'Shih Tzu'))
-    );
+    dog_id int primary key, 
+    name text unique,
+    breed text check (breed in ('Beagle', 'Labrador', 'Poodle', 'Shih Tzu')),
+    age int check(age>=1),
+    weight_kg float
+);
 
-insert into Dogs 
-Values (1, 'Buddy', 'Labrador', 3, 30.5),
-       (2, 'Lucy', 'Poodle', 5, 6.8),
-       (3, 'Max', 'Beagle', 2, 11.2);
+insert into Dogs
+values
+    (1, "Buddy", "Labrador", 3, 30.5),
+    (2, "Lucy", "Poodle", 5, 6.8),
+    (3, "Max", 'Beagle', 2, 11.2);
