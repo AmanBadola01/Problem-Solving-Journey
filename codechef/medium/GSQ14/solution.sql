abd67792f1c09b5aeb8d05170db30b6d727a076c
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-03 11:26:54 +0530

    ✅ GSQ14 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/GSQ14/solution.sql
+++ codechef/medium/GSQ14/solution.sql
@@ -1,10 +1,9 @@
-/* Write a query to create a table employee with the mentioned constraints on the columns : 
-employee_id - PRIMARY KEY, 
-employee_Name -UNIQUE, 
-department -NOT NULL CHECK (department IN ('Sales', 'Finance', 'Operations')) */
+/* Write a query which does the following
+- Delete the row where the department is Client.
+- Output the entire table.
+*/
 
-create table employee(
-    employee_id INT PRIMARY key, 
-    employee_Name text UNIQUE,
-    department text NOT NULL CHECK (department IN ('Sales', 'Finance', 'Operations'))
-)
+Delete from employee
+where Department = 'Client';
+
+select * from employee
