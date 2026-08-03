43c40a742489d69d87ccb753be40fa20c7ce5870
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-03 11:40:33 +0530

    ✅ CTMUXM02 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-03

---
+++ codechef/medium/CTMUXM02/solution.sql
@@ -0,0 +1,13 @@
+-- write your code here
+
+create table WorkshopSignup(
+    unique_id int primary key, 
+    email varchar Not null, 
+    workshop_type varchar(50) check (workshop_type in ('pottery', 'coding')),
+    signup_date date
+);
+
+insert into WorkshopSignup
+values (1, 'bob@example.com','coding', '2025-07-19'),
+(2, 'alice@example.com', 'pottery', '2025-07-18'),
+(3, 'bob@example.com', 'pottery', '2025-07-11');
