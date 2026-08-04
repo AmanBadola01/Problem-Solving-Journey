7abac3e85f38e0614ffbe3ffb4789127d4c075c5
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-04 10:21:34 +0530

    ✅ SQLPBP04 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-04

--- codechef/medium/SQLPBP04/solution.sql
+++ codechef/medium/SQLPBP04/solution.sql
@@ -1,5 +1,13 @@
 -- your code goes here
-select player_name, score 
-from Players
-order by score desc
-LIMIT 3
+select 
+    m.match_id, 
+    m.player_1, 
+    m.player_2, 
+    m.winner, 
+    m.match_date, 
+    p.score
+from Players p
+left join Matches m
+where p.player_name = m.winner
+order by match_date desc
+limit 5
