8b4f58abd93a2ef72cc8225cd298407ebdd78a27
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-05 12:47:58 +0530

    Time: 2 ms (98.05%), Space: 32.2 MB (68.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -6,9 +6,18 @@ class Solution:
         #             return True
         # return False
 
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
+        #Approach 2: 
+
+        # seen = set()
+        # for num in nums:
+        #     if num in seen:
+        #         return True
+        #     seen.add(num)
+        # return False
+
+        # Approach 3:
+        if len(nums) != len(set(nums)):
+            return True
+        else:
+            return False
+            
