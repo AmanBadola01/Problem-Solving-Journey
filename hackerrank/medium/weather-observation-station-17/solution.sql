527598f5fca4c53510c40df60a62aa78961cdbec
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-07 18:27:20 +0530

    ✅ Weather Observation Station 17 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-07

--- hackerrank/medium/weather-observation-station-17/solution.sql
+++ hackerrank/medium/weather-observation-station-17/solution.sql
@@ -2,7 +2,10 @@
 Enter your query here.
 */
 
-SELECT name 
-from Employee
-where salary > 2000 AND months < 10
-order by employee_id ASC;
+SELECT ROUND(LONG_W, 4)
+FROM STATION 
+WHERE LAT_N IN (
+    SELECT MIN(LAT_N) 
+    FROM STATION 
+    WHERE LAT_N > 38.7780
+)
