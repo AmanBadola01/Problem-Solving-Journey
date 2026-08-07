93b386cbd424867c45dcf903e02a623fb57079b9
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-07 18:11:24 +0530

    ✅ Weather Observation Station 12 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-07

---
+++ hackerrank/medium/weather-observation-station-12/solution.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LEFT(LOWER(CITY), 1) NOT IN ('a', 'e', 'i', 'o', 'u') AND
+      RIGHT(LOWER(CITY), 1) NOT IN ('a', 'e', 'i', 'o', 'u');
