499d5c542611e90ca9035de2ced9745fb82c773e
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-08 11:12:49 +0530

    ✅ Type of Triangle | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ hackerrank/medium/the-pads/solution.sql
@@ -0,0 +1,15 @@
+/*
+Enter your query here.
+*/
+
+SELECT CASE 
+    WHEN A+B > C AND  B+C > A AND A+C > B THEN 
+        CASE
+            WHEN A = B AND B = C THEN "Equilateral"
+            WHEN A = B OR B = C OR A = C THEN "Isosceles"
+            WHEN A <> B AND B <> C AND C <> A THEN "Scalene"
+        END
+    ELSE "Not A Triangle"
+END AS TRIANGLE_TYPE
+        
+FROM TRIANGLES ;
