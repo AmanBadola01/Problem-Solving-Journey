45792289accf0f552a834eafd3c5f293b6a74776
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-09 13:10:21 +0530

    Time: 1148 ms (88.13%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
-select name ,  unique_id 
-from Employees as em
-LEFT JOIN EmployeeUNI as emu
-ON em.id = emu.id
+select eu.unique_id, e.name
+from Employees e
+left join EmployeeUNI eu
+on e.id = eu.id
