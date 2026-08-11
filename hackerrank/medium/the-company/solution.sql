483650a6a88220dbbca14a9b1be52c345ff6b61f
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-11 09:11:00 +0530

    ✅ Binary Tree Nodes | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ hackerrank/medium/the-company/solution.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+*/
+SELECT 
+    N,
+    CASE 
+        WHEN P IS NULL THEN "Root"
+        WHEN N NOT IN (SELECT DISTINCT P FROM BST WHERE P IS NOT NULL) THEN "Leaf"
+        ELSE "Inner"
+    END AS node_type
+FROM BST
+ORDER BY N ASC
