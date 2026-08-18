fc6102b9e5efaa084e2242f909a4c0ff46daa8f6
Aman Badola <163629393+AmanBadola01@users.noreply.github.com>
2026-08-18 09:52:10 +0530

    ✅ Weather Observation Station 20 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-18

--- hackerrank/medium/weather-observation-station-20/solution.sql
+++ hackerrank/medium/weather-observation-station-20/solution.sql
@@ -1,15 +1,16 @@
--- /*
--- Enter your query here.
--- */
+/*
+Enter your query here.
+*/
 
--- a,b are the respective minimum and maximum values of Northern Latitude (LAT_N) 
-
--- c,d are the respective minimum and maximum values of Western Longitude (LONG_W) 
-
-SELECT ROUND(
-    SQRT(
-        POWER(MAX(LAT_N) - MIN(LAT_N), 2) +
-        POWER(MAX(LONG_W) - MIN(LONG_W), 2)
-    ),4
+WITH temp as (
+    select LAT_N, 
+    ROW_NUMBER() over (ORDER BY LAT_N) as rn,
+    count(*) over() as total
+    from STATION
 )
-FROM STATION;
+
+select round(avg(LAT_N), 4) as lat_median
+from temp 
+where rn in (
+    FLOOR((total + 1) /2), CEIL((total + 1) / 2)
+    )
